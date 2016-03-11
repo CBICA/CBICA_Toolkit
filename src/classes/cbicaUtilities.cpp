f8b984c26970317bc5a74932c21105d2584d9d39
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2016-03-11 22:50:30 +0000

    fixed bugs
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/CBICA_Toolkit/trunk@346 36980a1d-918e-4cb2-ab0a-7661cd27ea28

--- src/classes/cbicaUtilities.cpp
+++ src/classes/cbicaUtilities.cpp
@@ -1044,67 +1044,74 @@ namespace cbica
         extension = tempExt + compressionFormats[i];
       }
     }
+    if (!path.empty() && !baseName.empty() && !extension.empty())
+    {
+      return true;
+    }
+    else
+    {
       //! Initialize pointers to file and user names
 #if (_MSC_VER >= 1700)
-    char basename_var[FILENAME_MAX], ext[FILENAME_MAX], path_name[FILENAME_MAX], drive_letter[FILENAME_MAX];
-    //_splitpath(dataFile_wrap.c_str(), NULL, path_name, basename_var, ext);
-    _splitpath_s(dataFile.c_str(), drive_letter, FILENAME_MAX, path_name, FILENAME_MAX, basename_var, FILENAME_MAX, ext, FILENAME_MAX);
+      char basename_var[FILENAME_MAX], ext[FILENAME_MAX], path_name[FILENAME_MAX], drive_letter[FILENAME_MAX];
+      //_splitpath(dataFile_wrap.c_str(), NULL, path_name, basename_var, ext);
+      _splitpath_s(dataFile.c_str(), drive_letter, FILENAME_MAX, path_name, FILENAME_MAX, basename_var, FILENAME_MAX, ext, FILENAME_MAX);
 #else
-    char *basename_var, *ext, *path_name;
-    path_name = dirname(cbica::constCharToChar(dataFile_wrap.c_str()));
-    basename_var = basename(cbica::constCharToChar(dataFile_wrap.c_str()));
-    ext = strrchr(cbica::constCharToChar(dataFile_wrap.c_str()), '.');
+      char *basename_var, *ext, *path_name;
+      path_name = dirname(cbica::constCharToChar(dataFile_wrap.c_str()));
+      basename_var = basename(cbica::constCharToChar(dataFile_wrap.c_str()));
+      ext = strrchr(cbica::constCharToChar(dataFile_wrap.c_str()), '.');
 #endif
 
-    //path sanity check
-    if (path_name == NULL)
-    {
-      std::cerr << "No filename path has been detected.\n";
-      exit(EXIT_FAILURE);
-    }
-    else
-    {
-      path = 
+      //path sanity check
+      if (path_name == NULL)
+      {
+        std::cerr << "No filename path has been detected.\n";
+        exit(EXIT_FAILURE);
+      }
+      else
+      {
+        path =
 #ifdef _WIN32
-        std::string(drive_letter) + 
+          std::string(drive_letter) +
 #endif
-        std::string(path_name);
-    }
-    path = cbica::replaceString(path, "\\", "/"); // normalize path for Windows
+          std::string(path_name);
+      }
+      path = cbica::replaceString(path, "\\", "/"); // normalize path for Windows
 
-    //base name sanity check
-    if (basename_var == NULL)
-    {
-      std::cerr << "No filename base has been detected.\n";
-      exit(EXIT_FAILURE);
-    }
-    else
-    {
-      baseName = std::string(basename_var);
-    }
+      //base name sanity check
+      if (basename_var == NULL)
+      {
+        std::cerr << "No filename base has been detected.\n";
+        exit(EXIT_FAILURE);
+      }
+      else
+      {
+        baseName = std::string(basename_var);
+      }
 
-    //extension sanity check
-    if (ext == NULL)
-    {
-      extension = "";
-    }
-    else
-    {
-      extension = std::string(ext);
-    }
-    
+      //extension sanity check
+      if (ext == NULL)
+      {
+        extension = "";
+      }
+      else
+      {
+        extension = std::string(ext);
+      }
+
 #if (_MSC_VER >= 1700)
-    path_name[0] = NULL;
-    basename_var[0] = NULL;
-    ext[0] = NULL;
-    drive_letter[0] = NULL;
+      path_name[0] = NULL;
+      basename_var[0] = NULL;
+      ext[0] = NULL;
+      drive_letter[0] = NULL;
 #endif
-    if (path[path.length() - 1] != '/')
-    {
-      path += "/";
+      if (path[path.length() - 1] != '/')
+      {
+        path += "/";
+      }
+
+      return true;
     }
-    
-    return true;
   }
 
   std::vector<std::string> stringSplit( const std::string &str, const std::string &delim )
