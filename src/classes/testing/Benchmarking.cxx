1d3ce726aff191a5d34d4e3230ceda3cd0d34c1a
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2016-07-13 13:38:49 +0000

    a basic shell for Benchmarking is ready
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/CBICA_Toolkit/trunk@433 36980a1d-918e-4cb2-ab0a-7661cd27ea28

--- src/classes/testing/Benchmarking.cxx
+++ src/classes/testing/Benchmarking.cxx
@@ -24,7 +24,7 @@ int main(int argc, char** argv)
   parser.addRequiredParameter("i", "inputPatterns", cbica::Parameter::STRING, "comma separated entries", "String pattern(s) for the input files");
   parser.addRequiredParameter("o", "outputPatterns", cbica::Parameter::STRING, "comma separated entries", "String pattern(s) for the output files");
   parser.addOptionalParameter("L", "LogFile", cbica::Parameter::FILE, "text file", "File to write all logging information", "Defualts to cbica::makeTempDir()/Benchmarking.log");
-  parser.exampleUsage("-d C:/here/is/my/Data/ -i fixedImage,movingImage -o output");
+  parser.exampleUsage("-d C:/here/is/my/Data/ -i fixed,moving -o output");
 
   std::string logFile = cbica::createTmpDir() + "Benchmarking.log", inputPatterns, outputPatterns, dataDir;
 
@@ -58,8 +58,54 @@ int main(int argc, char** argv)
   {
     const std::string subjectUnderConsideration = dataDir + detectedSubjects[i] + "/";
     std::vector< std::string > filesPerSubject = cbica::filesInDirectory(subjectUnderConsideration);
+    std::vector< std::string > inputFiles, outputFiles;
+    std::vector< size_t > outputFileIndeces;
 
-    int blah = 1;
+    for (size_t j = 0; j < filesPerSubject.size(); j++)
+    {
+      const std::string fileUnderConsideration = subjectUnderConsideration + filesPerSubject[j];
+      
+      // ensure file is present
+      if (!cbica::fileExists(fileUnderConsideration))
+      {
+        logger.WriteError("The file, '" + fileUnderConsideration + "' wasn't found");
+        return EXIT_FAILURE;
+      }
+
+      // check for output file patterns
+      for (size_t k = 0; k < outputPatterns_vector.size(); k++)
+      {
+        if (fileUnderConsideration.find(outputPatterns_vector[k]) != std::string::npos)
+        {
+          outputFiles.push_back(fileUnderConsideration);
+          outputFileIndeces.push_back(j);
+        }
+      }
+
+    }
+
+    // the reason why input and output patterns are being processes separately is to take care of the possibility of encountering a file like 'movingImage_T1_processed_output.nii.gz'
+    for (size_t j = 0; j < filesPerSubject.size(); j++)
+    {
+      if (std::find(outputFileIndeces.begin(), outputFileIndeces.end(), j) == outputFileIndeces.end())
+      {
+        const std::string fileUnderConsideration = subjectUnderConsideration + filesPerSubject[j]; // check is not needed since it is already done in the previous loop
+        // check for input file patterns
+        for (size_t k = 0; k < inputPatterns_vector.size(); k++)
+        {
+          if (fileUnderConsideration.find(inputPatterns_vector[k]) != std::string::npos)
+          {
+            inputFiles.push_back(fileUnderConsideration);
+          }
+        }
+      }
+    }
+
+    // run the algorithm which needs to be tested on the inputFiles and save the result(s) [preferably using one of the outputPatterns]
+
+    // run comparisons between the different output files and generate statistics. Save those statistics using logger
   }
 
+  logger.Write("Finished Successfully");
+  return EXIT_SUCCESS;
 }
