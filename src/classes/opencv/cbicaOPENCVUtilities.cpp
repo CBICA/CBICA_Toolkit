abaaec61c5d5a77beaa9a4693f74dd7bf37eb692
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2017-06-08 15:16:34 +0000

    slight restructuring of the roc function
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/CBICA_Toolkit/trunk@539 36980a1d-918e-4cb2-ab0a-7661cd27ea28

--- src/classes/opencv/cbicaOPENCVUtilities.cpp
+++ src/classes/opencv/cbicaOPENCVUtilities.cpp
@@ -20,13 +20,21 @@ namespace cbica
     }
 
     std::vector< float > realLabels, predictedLabels;
-    for (size_t i = 0; i < inputData.rows; i++)
+    for (int i = 0; i < inputData.rows; i++)
     {
       realLabels.push_back(inputData.ptr< float >(i)[0]);
       predictedLabels.push_back(inputData.ptr< float >(i)[1]);
     }
 
     return cbica::ConfusionMatrix(realLabels, predictedLabels); // call ROC from cbica Utilities
+  }
+
+  std::map< std::string, float > ROC_Values(const cv::Mat &inputRealValues, const cv::Mat &inputPredictedValues)
+  {
+    std::vector< float > realLabels = GetVectorFromMat(inputRealValues),
+      predictedLabels = GetVectorFromMat(inputPredictedValues);
+
+    return cbica::ROC_Values(realLabels, predictedLabels); // call ROC from cbica Utilities
   }
 
   std::map< std::string, float > ROC_Values(const cv::Mat &inputRealAndPredictedLabels)
@@ -46,54 +54,25 @@ namespace cbica
       inputData.convertTo(inputData, depthCondition);
     }
 
-    std::vector< float > realLabels, predictedLabels;
-    realLabels.resize(inputData.rows);
-    predictedLabels.resize(inputData.rows);
-    for (size_t i = 0; i < inputData.rows; i++)
-    {
-      realLabels[i] = inputData.ptr< float >(i)[0];
-      predictedLabels[i] = inputData.ptr< float >(i)[1];
-    }
-
-    return cbica::ROC_Values(realLabels, predictedLabels); // call ROC from cbica Utilities
+    return ROC_Values(inputData.col(0), inputData.col(1));
   }
 
-  cv::Mat Z_Scores(const cv::Mat &inputVector)
+  cv::Mat Z_Scores(const cv::Mat &inputMat)
   {
-    cv::Mat inputData = inputVector;
-    // consider the case of a column-major data being provided
-    if (inputData.cols > inputData.rows)
-    {
-      inputData = inputData.t();
-    }
-
-    auto depthCondition = CV_32F;
-    if (inputData.depth() != depthCondition)
-    {
-      //std::cerr << "Changing depth of input to CV_32F\n";
-      inputData.convertTo(inputData, depthCondition);
-    }
+    auto inputData = GetVectorFromMat< float >(inputMat);
 
     // construct the returnMat with the appropriate size
-    cv::Mat returnMat = cv::Mat::zeros(inputData.size(), inputData.depth());
-
-    // populate data in a standard data structure
-    std::vector< float > actualData;
-    actualData.resize(inputData.rows);
-    for (size_t i = 0; i < inputData.rows; i++)
-    {
-      actualData[i] = inputData.ptr< float >(i)[0];
-    }
+    cv::Mat returnMat = cv::Mat::zeros(inputMat.size(), inputMat.depth());
 
     // get output from cbica::Utilities
-    auto outputZScores = cbica::ZScores(actualData);
+    auto outputZScores = cbica::ZScores(inputData);
     
     // popoulate returnMat with the output
-    for (size_t i = 0; i < returnMat.rows; i++)
+    for (int i = 0; i < returnMat.rows; i++)
     {
       returnMat.ptr< float >(i)[0] = outputZScores[i];
     }
 
-    return returnMat;
+    return GetMatFromVector(outputZScores, inputMat.size());
   }
 }
--- src/classes/opencv/cbicaOPENCVUtilities.h
+++ src/classes/opencv/cbicaOPENCVUtilities.h
@@ -37,12 +37,7 @@ namespace cbica
   /**
   \brief Calculates the Confusion Matrix for a set of real and predicted labels
 
-  Values returned: 
-  True Positive (TP), False Positive (FP), True Negative (TN), False Negative (FN), Real Positive (RP), Preditcted Positive (PP) [all from ConfusionMatrix()],
-  Accuracy, Positive Predictive Value (PPV) [aka Precision], False Discovery Rate (FDR), False Omission Rate (FOR), 
-  Negative Predictive Value (NPV), Prevalence, True Positive Rate (TPR) [aka Sensitivity, Recall, Probability of Detection (POD)], 
-  False Positive Rate (FPR) [aka Fall-out], False Negative Rate (FNR) [aka Miss Rate],  True Negative Rate (TNR) [aka Specificity], 
-  Positive Likelihood Ratio (LR+), Negative Likelihood Ratio (LR−), Diagnostic Odds Ratio (DOR)
+  Calls cbica::Utilities::Roc_Values inside; see that function for details.
 
   \param inputRealAndPredictedLabels A 2xN or Nx2 matrix containing real labels in the first column/row and then the predicted labels
 
@@ -51,9 +46,68 @@ namespace cbica
   std::map< std::string, float > ROC_Values(const cv::Mat &inputRealAndPredictedLabels);
 
   /**
+  \brief Calculates the Confusion Matrix for a set of real and predicted labels
+
+  Calls cbica::Utilities::Roc_Values inside; see that function for details.
+
+  \param inputRealValues A 1xN or Nx1 matrix containing real labels 
+  \param inputPredictedValues A 1xN or Nx1 matrix containing predicted labels 
+
+  \return std::map< string, float > A map of string and corresponding float values
+  */
+  std::map< std::string, float > ROC_Values(const cv::Mat &inputRealValues, const cv::Mat &inputPredictedValues);
+
+  /**
   \brief Calculate z-scores for the supplied vector
 
   \param inputVector Vector in a cv::Mat structure
   */
-  cv::Mat ZScores(const cv::Mat &inputVector);
+  cv::Mat ZScores(const cv::Mat &inputMat);
+
+  /**
+  \brief Convert a 1-channel cv::Mat to a std::vector
+  */
+  template< class TPixelType = float >
+  std::vector< TPixelType > GetVectorFromMat(const cv::Mat& inputMat)
+  {
+    std::vector< TPixelType > returnVector;
+
+    if (inputMat.isContinuous())
+    { 
+      returnVector.assign((TPixelType*)inputMat.datastart, (TPixelType*)inputMat.dataend);
+    }
+    else 
+    {
+      for (int i = 0; i < inputMat.rows; ++i)
+      {
+        returnVector.insert(returnVector.end(), inputMat.ptr< TPixelType >(i), inputMat.ptr< TPixelType >(i) + inputMat.cols);
+      }
+    }     
+    return returnVector;
+  }
+  
+  /**
+  \brief Convert an std::vector to a 1-channel cv::Mat
+
+  \param inputVector The input vector that needs to be converted to cv::Mat
+  \param outputSize The expected size of the output cv::Mat (used to call reshape inside); defaults to [1xinputVector.size()]
+  */
+  template< class TPixelType = float >
+  cv::Mat GetMatFromVector(const std::vector< TPixelType >& inputVector, const cv::Size &outputSize)
+  {
+    cv::Mat returnMat;
+
+    // auto initialization doesn't work inside the for-loop
+    auto it = inputVector.begin();
+    auto jt = returnMat.begin< TPixelType >();
+
+    for (; it != inputVector.end(); ++it)
+    { 
+      *jt++ = static_cast< TPixelType >(*it);
+    }
+
+    returnMat.reshape(1, outputSize.height);
+
+    return returnMat;
+  }
 }
