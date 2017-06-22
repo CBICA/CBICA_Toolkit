2087d50391010afdd1de680c1913c97c41182225
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2017-06-22 15:15:27 +0000

    deformable registration function added; minor alterations in the function signatures
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/CBICA_Toolkit/trunk@554 36980a1d-918e-4cb2-ab0a-7661cd27ea28

--- src/classes/itk/cbicaITKUtilities.h
+++ src/classes/itk/cbicaITKUtilities.h
@@ -3,7 +3,7 @@
 
 \brief Some basic utility functions.
 
-Dependecies: ITK, OpenMP
+Dependecies: ITK (module_review enabled), OpenMP
 
 https://www.cbica.upenn.edu/sbia/software/ <br>
 software@cbica.upenn.edu
@@ -22,10 +22,32 @@ See COPYING file or https://www.cbica.upenn.edu/sbia/software/license.html
 #include "itkImageRegionIterator.h"
 #include "itkHistogramMatchingImageFilter.h"
 #include "itkAdaptiveHistogramEqualizationImageFilter.h"
+#include "itkMultiResolutionPDEDeformableRegistration.h"
+#include "itkDemonsRegistrationFilter.h"
+#include "itkDiffeomorphicDemonsRegistrationFilter.h"
+#include "itkFastSymmetricForcesDemonsRegistrationFilter.h"
+#include "itkSymmetricForcesDemonsRegistrationFilter.h"
+
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkBSplineInterpolateImageFunction.h"
+#include "itkNearestNeighborInterpolateImageFunction.h"
+
 
 #include "gdcmMD5.h"
 
-typedef itk::Image<double, 3> TImageType;
+using ImageTypeFloat3D = itk::Image< float, 3 >;
+unsigned int RmsCounter = 0;
+double MaxRmsE[4] = { 0.8, 0.75, 0.4, 0.2 };
+
+enum DeformRegType
+{
+  Demons, DiffeomorphicDemons, SymmetricForcesDemons, FastSymmetricForcesDemons
+};
+
+enum InterpolatorType
+{
+  Linear, NearestNeighbor, BSpline
+};
 
 namespace cbica
 {
@@ -35,7 +57,7 @@ namespace cbica
   \param inputModalitiesAndImages A collection of images which are stored in a per-modality basis (each entry corresponds to a subject, whose entries contain different modalities)
   \return A collection of indeces which constitute the non-zero locations per modality (each entry corresponds to a subject, which contains the locations of non-zero pixel values for all modalities)
   */
-  template< class TImageType = itk::Image< float, 3 > >
+  template< class TImageType = ImageTypeFloat3D >
   std::vector< std::vector< typename TImageType::IndexType > > CreateMaskIndeces(const std::vector< std::vector< typename TImageType::Pointer > > &inputModalitiesAndImages)
   {
     std::vector< std::vector< typename TImageType::IndexType > > returnMaskIndeces;
@@ -101,7 +123,7 @@ namespace cbica
   \param indeced The indeces from which pixel values need to be extracted
   \return Vector of values whose data type is the same as image type
   */
-  template < typename TImageType = itk::Image< float, 3 > >
+  template < typename TImageType = ImageTypeFloat3D >
   std::vector< typename TImageType::PixelType > GetPixelValuesFromIndeces(const typename TImageType::Pointer inputImage, const std::vector< typename TImageType::IndexType > &indeces)
   {
     std::vector< typename TImageType::PixelType > returnVector;
@@ -126,11 +148,12 @@ namespace cbica
   /**
   \brief Wrap of GetPixelValues
   */
-  template < typename TImageType = itk::Image< float, 3 > >
+  template < typename TImageType = ImageTypeFloat3D >
   std::vector< typename TImageType::PixelType > ExtractPixelValuesFromIndeces(const typename TImageType::Pointer inputImage, const std::vector< typename TImageType::IndexType > &indeces)
   {
     return GetPixelValuesFromIndeces< TImageType >(inputImage, indeces);
   }
+  
   /**
   \brief Get MD5 sum of a supplied file
 
@@ -156,7 +179,7 @@ namespace cbica
   /**
   \brief Get the indeces of the image which are not zero
   */
-  template < typename TImageType = itk::Image< float, 3 > >
+  template < typename TImageType = ImageTypeFloat3D >
   std::vector< typename TImageType::IndexType > GetIndexFromNonZeroPixels(const typename TImageType::Pointer inputImage, const std::string valuesToExclude = "0")
   {
     std::vector< typename TImageType::IndexType > returnVector;
@@ -181,7 +204,7 @@ namespace cbica
   \param numberOfMatchPoints Governs the number of quantile values to be matched
   \param numberOfHistogramLevels Sets the number of bins used when creating histograms of the source and reference images
   */
-  template < typename TImageType = itk::Image< float, 3 > >
+  template < typename TImageType = ImageTypeFloat3D >
   typename TImageType::Pointer GetHistogramMatchedImage(const typename TImageType::Pointer inputImage, const typename TImageType::Pointer referenceImage, 
     const int numberOfMatchPoints = 40, const int numberOfHistogramLevels = 100)
   {
@@ -208,7 +231,7 @@ namespace cbica
   \param beta Ranges between 0-1; with 1 giving result same as input image and lower values behaving as unsharp filters; default = 0.3
   \param radius Ranges between 1-10 with default = 1
   */
-  template < typename TImageType = itk::Image< float, 3 > >
+  template < typename TImageType = ImageTypeFloat3D >
   typename TImageType::Pointer GetAdaptiveHistogramEqualizedImage(const typename TImageType::Pointer inputImage, const typename TImageType::Pointer referenceImage,
     const float alpha = 0.3, const float beta = 0.3, const float radius = 1, const int numberOfHistogramLevels = 100)
   {
@@ -221,4 +244,136 @@ namespace cbica
 
     return filter->GetOutput();
   }
+
+  /**
+  \brief Perform the deformable registration
+
+  \param movingImage The moving image for registration
+  \param referenceImage The reference image for registration
+  \param multiResLevels Number of multi-resolution levels for registration, defaults to 5
+  \param iterationStart Start size of iteration for first multiResLevel, defaults to 10
+  \param iterationStep Step size of the iterations increasing over each MultiResLevel, defaults to 10
+  \param iterationEnd End size of iteration for first multiResLevel, defaults to 50
+  \param regType The type of registration to perform, defaults to 'Demons'
+  \param interpolatorType The type of interpolator to use, defaults to 'Linear'
+  */
+  template< class TImageType = ImageTypeFloat3D >
+  typename TImageType::Pointer GetDeformRegisteredImage(const typename TImageType::Pointer movingImage, const typename TImageType::Pointer referenceImage,
+    const unsigned int multiResLevels = 5, 
+    const unsigned int iterationStart = 10, const unsigned int iterationStep = 10, const unsigned int iterationEnd = 50,
+    const int regType = Demons, const int interpolatorType = Linear)
+  {
+    // do histogram matchin of the 2 images
+    auto movingImage_histoMatched = GetHistogramMatchedImage< TImageType >(movingImage, referenceImage, 40, 1024);
+
+    // setup the displacement field
+    using VectorPixelType = itk::Vector< float, TImageType::ImageDimension >;
+    using DisplacementFieldType =  itk::Image< VectorPixelType, TImageType::ImageDimension >;
+
+    auto multiRes = typename itk::MultiResolutionPDEDeformableRegistration< TImageType, TImageType, DisplacementFieldType >::New();
+
+    // set the registration type
+    switch (regType)
+    {
+    case DiffeomorphicDemons:
+    {
+      auto filter = typename itk::DiffeomorphicDemonsRegistrationFilter< TImageType, TImageType, DisplacementFieldType >::New();
+      filter->SetStandardDeviations(1.0);
+      multiRes->SetRegistrationFilter(filter);
+      break;
+    }
+    case SymmetricForcesDemons:
+    {
+      auto filter = typename itk::SymmetricForcesDemonsRegistrationFilter< TImageType, TImageType, DisplacementFieldType >::New();
+      filter->SetStandardDeviations(1.0);
+      multiRes->SetRegistrationFilter(filter);
+      break;
+    }
+    case FastSymmetricForcesDemons:
+    {
+      auto filter = typename itk::FastSymmetricForcesDemonsRegistrationFilter< TImageType, TImageType, DisplacementFieldType >::New();
+      filter->SetStandardDeviations(1.0);
+      multiRes->SetRegistrationFilter(filter);
+      break;
+    }
+    default: // does Demons
+    {
+      auto filter = typename itk::DemonsRegistrationFilter< TImageType, TImageType, DisplacementFieldType >::New();
+      filter->SetStandardDeviations(1.0);
+      multiRes->SetRegistrationFilter(filter);
+      break;
+    }
+    }
+
+    // set the different parameters of the multi resolution registration
+    multiRes->SetNumberOfLevels(multiResLevels);
+    multiRes->SetFixedImage(referenceImage);
+    multiRes->SetMovingImage(movingImage_histoMatched);
+
+    // set up the iterations
+    std::vector< unsigned int > iterations_vector;
+    for (size_t i = iterationStart; i <= iterationEnd; i += iterationStep)
+    {
+      iterations_vector.push_back(i);
+    }
+    unsigned int *iterations_array = &iterations_vector[0];
+    multiRes->SetNumberOfIterations(iterations_array);
+
+    // start the regisration
+    try
+    {
+      multiRes->Update();
+    }
+    catch (itk::ExceptionObject & excp)
+    {
+      std::cerr << excp << std::endl;
+      return referenceImage;
+    }
+    
+    // warp the moving image
+    auto warper = typename itk::WarpImageFilter< TImageType, TImageType, DisplacementFieldType >::New();
+    warper->SetInput(movingImage);
+    warper->SetOutputSpacing(referenceImage->GetSpacing());
+    warper->SetOutputOrigin(referenceImage->GetOrigin());
+    warper->SetOutputDirection(referenceImage->GetDirection());
+    warper->SetDisplacementField(multiRes->GetOutput());
+
+    // set up the interpolator type
+    switch (interpolatorType)
+    {
+    case NearestNeighbor:
+    {
+      auto interpolator = typename itk::NearestNeighborInterpolateImageFunction< TImageType, double >::New();
+      warper->SetInterpolator(interpolator);
+      break;
+    }
+    case BSpline:
+    {
+      auto interpolator = typename itk::BSplineInterpolateImageFunction< TImageType, double >::New();
+      warper->SetInterpolator(interpolator);
+      break;
+    }
+    default: // linear by default
+    {
+      auto interpolator = typename itk::LinearInterpolateImageFunction< TImageType, double >::New();
+      warper->SetInterpolator(interpolator);
+      break;
+    }
+    }
+
+    // perform the warping
+    try
+    {
+      warper->Update();
+    }
+    catch (itk::ExceptionObject & excp)
+    {
+      std::cerr << excp << std::endl;
+      return referenceImage;
+    }
+
+    return warper->GetOutput();
+  }
+
+
 }
