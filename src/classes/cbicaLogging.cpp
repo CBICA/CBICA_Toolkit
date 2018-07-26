d2a6649f0a395b403e37ce831eb1146d77cd61f0
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2018-07-26 18:49:47 +0000

    added a check for logging
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/CBICA_Toolkit/trunk@692 36980a1d-918e-4cb2-ab0a-7661cd27ea28

--- src/classes/cbicaLogging.cpp
+++ src/classes/cbicaLogging.cpp
@@ -97,12 +97,16 @@ namespace cbica
   void Logging::Write(const std::string FreeText_input = "")
   { 
     // assumes file exists because constructor writes the file once
-    log_file.open(file_name_with_path.c_str(), std::ios_base::app);
-    while (!log_file.is_open())
+    if (!file_name_with_path.empty())
     {
-      cbica::sleep(100);
       log_file.open(file_name_with_path.c_str(), std::ios_base::app);
+      while (!log_file.is_open())
+      {
+        cbica::sleep(100);
+        log_file.open(file_name_with_path.c_str(), std::ios_base::app);
+      }
     }
+
     if (FreeText_input.empty())
     {
       writing_function("", log_file, exe_name, user_name);
