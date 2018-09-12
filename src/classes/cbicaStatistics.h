acfcd21668342ad5dfac85089803220df5509a74
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2018-09-12 13:52:08 +0000

    added new functions
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/CBICA_Toolkit/trunk@697 36980a1d-918e-4cb2-ab0a-7661cd27ea28

--- src/classes/cbicaStatistics.h
+++ src/classes/cbicaStatistics.h
@@ -74,14 +74,69 @@ namespace cbica
       return m_variance;
     }
 
+    TDataType GetMode()
+    {
+      if (!mode_calculated)
+      {
+        m_mode = m_input_Sorted[0];
+        auto number = m_input_Sorted[0];
+        int count = 1;
+        int countMode = 1;
+
+        for (size_t i = 1; i < m_input_Sorted.size(); i++)
+        {
+          if (m_input_Sorted[i] == number)
+          { // count occurrences of the current number
+            ++count;
+          }
+          else
+          { // now this is a different number
+            if (count > countMode)
+            {
+              countMode = count; // mode is the biggest ocurrences
+              m_mode = number;
+            }
+            count = 1; // reset count for the new number
+            number = m_input_Sorted[i];
+          }
+        }
+        mode_calculated = true;
+      }
+
+      return m_mode;
+    }
+
+    TDataType GetMedian()
+    {
+      if (!median_calculated)
+      {
+        if (!m_input_Sorted.empty())
+        {
+          auto size = m_input_Sorted.size();
+          if (size % 2 == 0)
+          {
+            m_median = (m_input_Sorted[size / 2 - 1] + m_input_Sorted[size / 2]) / 2;
+          }
+          else
+          {
+            m_median = m_input_Sorted[size / 2];
+          }
+        }
+        else
+        {
+          std::cerr << "Array cannot be empty.\n";
+          return std::numeric_limits< TDataType >::min();
+        }
+        median_calculated = true;
+      }
+
+      return m_median;
+    }
+
     //! Does exactly what it says
     double GetStandardDeviation()
     {
-      if (!stdDev_calculated)
-      {
-        m_stdDev = std::sqrt(static_cast<double>(m_variance));
-        stdDev_calculated = true;
-      }
+      CalculateSTD();
       return m_stdDev;
     }
 
@@ -90,11 +145,7 @@ namespace cbica
     {
       if (!kurtosis_calculated)
       {
-        if (!stdDev_calculated)
-        {
-          m_stdDev = static_cast<double>(m_variance);
-          stdDev_calculated = true;
-        }
+        CalculateSTD();
 
         m_kurtosis = weirdStatistics(4);
         kurtosis_calculated = true;
@@ -108,11 +159,7 @@ namespace cbica
     {
       if (!skewness_calculated)
       {
-        if (!stdDev_calculated)
-        {
-          m_stdDev = static_cast<double>(m_variance);
-          stdDev_calculated = true;
-        }
+        CalculateSTD();
 
         m_skewness = weirdStatistics(3);
         skewness_calculated = true;
@@ -139,23 +186,32 @@ namespace cbica
 
   private:
     std::vector< TDataType > m_input;
+    std::vector< TDataType > m_input_Sorted;
 
     //! actual outputs
     double m_sum = 0.0, m_mean = 0.0, m_variance = 0.0, m_stdDev = 0.0, m_kurtosis = 0.0, m_skewness = 0.0, m_max = 0.0, m_min = 0.0;
 
+    TDataType m_mode, m_median;
+
     std::vector< double > m_zscores;
 
     //! flags to check if something has been calculated or not
-    bool stdDev_calculated = false, kurtosis_calculated = false, skewness_calculated = false, zscores_calculated = false;
+    bool stdDev_calculated = false, kurtosis_calculated = false, skewness_calculated = false, zscores_calculated = false, 
+      mode_calculated = false, median_calculated = false;
 
     //! This function gets called every time the input is set, for obvious reasons
     void InitializeClass(std::vector< TDataType >& inputArray)
     {
       m_input = inputArray;
+      m_input_Sorted = inputArray;
+      std::sort(m_input_Sorted.begin(), m_input_Sorted.end());
+
       stdDev_calculated = false;
       kurtosis_calculated = false;
       skewness_calculated = false;
       zscores_calculated = false;
+      mode_calculated = false; 
+      median_calculated = false;
       m_sum = std::accumulate(m_input.begin(), m_input.end(), 0.0);
       m_mean = m_sum / m_input.size();
       auto result = std::minmax_element(m_input.begin(), m_input.end());
@@ -178,6 +234,16 @@ namespace cbica
         returnStat += std::pow(((m_input[x] - m_mean) / m_stdDev), power);
       }
       return (returnStat / m_input.size());
+    }
+
+    //! Single place to calculate STD Dev to reduce number of moving parts
+    void CalculateSTD()
+    {
+      if (!stdDev_calculated)
+      {
+        m_stdDev = std::sqrt(static_cast<double>(m_variance));
+        stdDev_calculated = true;
+      }
     }
   };
 
