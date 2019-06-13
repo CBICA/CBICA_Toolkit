37b1f7aae8301a61edefb9d2c521fa549ba746fb
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-06-13 09:32:08 -0400

    project name changes

--- docs/conf.py
+++ docs/conf.py
@@ -32,18 +32,18 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 breathe_projects = {}
 if read_the_docs_build:
-	input_dir = '../CatCutifier'
+	input_dir = '../src'
 	output_dir = 'build'
 	configureDoxyfile(input_dir, output_dir)
 	subprocess.call('doxygen', shell=True)
-	breathe_projects['CatCutifier'] = output_dir + '/xml'
+	breathe_projects['CBICAToolkit'] = output_dir + '/xml'
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'CatCutifier'
-copyright = '2019, Simon Brand'
-author = 'Simon Brand'
+project = 'CBICAToolkit'
+copyright = '2019, Center for Biomedical Image Computing and Analytics'
+author = 'CBICA Software'
 
 
 # -- General configuration ---------------------------------------------------
@@ -79,4 +79,4 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # Breathe Configuration
-breathe_default_project = "CatCutifier"
+breathe_default_project = "CBICAToolkit"
